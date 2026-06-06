cde1658aa0118070b7accd51bd3d702848d0f9d8
Alejandro Caceres <cacp124@gmail.com>
2026-06-05 23:28:29 -0400

    Add main functionality and requirements for Spotify Likes Tool

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Load your credentials from the .env file
+dotenv.load_dotenv(dotenv_path="Credentials/credentials.env")
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    redirect_uri="http://127.0.0.1:8000/callback",
+    scope="user-library-read"
+))
+
+results = sp.current_user_saved_tracks(limit=20, offset=0)
+for item in results['items']:
+    track = item['track']
+    print(track['artists'][0]['name'], "-", track['name'])
